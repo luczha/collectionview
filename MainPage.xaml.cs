a75c2e52fd925565cb493c9006d2903792099ab0
luczha <47239772+luczha@users.noreply.github.com>
2020-02-05 19:19:34 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+namespace App7
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+
+            
+
+            InitializeComponent();
+
+           
+
+        }
+    }
+
+    public class MyModel
+    {
+        public string FormattedDate { get; set; }
+        public string TitleText { get; set; }
+    }
+}
